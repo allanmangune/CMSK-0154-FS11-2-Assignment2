b4cb88f81d00dd9a5d0af4903aeac3c6577ba60f
allan <allanmangune@hotmail.com>
2024-03-14 19:01:29 -0600

    Completed assignment.

---
+++ Assignment2/Program.cs
@@ -0,0 +1,23 @@
+﻿using Assignment2;
+
+Store store = new Store();
+store.Data = new List<int>() { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+int evenNumbers = 0;
+int oddNumbers = 0;
+
+foreach (var item in store.Data)
+{
+    if (item % 2 == 1)
+    {
+        Console.WriteLine($"{item} is an odd number");
+        oddNumbers = oddNumbers + item;
+    }
+    else
+    {
+        Console.WriteLine($"{item} is an even number");
+        evenNumbers = evenNumbers + item;
+    }
+}
+Console.WriteLine($"Total even numbers: {evenNumbers}");
+Console.WriteLine($"Total odd numbers: {oddNumbers}");
